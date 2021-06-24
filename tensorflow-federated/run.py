a388c9203b38e6b2d0f873debe757f721d4fc340
Louis Mullie <louis.mullie@gmail.com>
2021-06-23 21:08:30 -0400

    More comments

--- tensorflow-federated/run.py
+++ tensorflow-federated/run.py
@@ -4,36 +4,30 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_federated as tff
 
-# Helper functions
+# 
+#  Configure training task, preprocess data, plan task (/prepare)
+# 
+
+NUM_CLIENTS = 10
+BATCH_SIZE = 20
+
+OPTIMIZER = 'SGD'
+LOSS_FUNCTION = 'SparseCategoricalCrossentropy'
+METRIC_FUNCTION = 'SparseCategoricalAccuracy'
+
+# Get the model structure JSON
+with open('model.json','r') as file:
+  MODEL_JSON_CONFIG = file.read()
+
+# Preprocess EMNIST data.
 def preprocess(dataset, batch_size):
 
   # Flatten a batch of EMNIST data and return a (features, label) tuple.
   def batch_format_fn(element):
-    """"""
     return (tf.reshape(element['pixels'], [-1, 784]), 
             tf.reshape(element['label'], [-1, 1]))
 
   return dataset.batch(batch_size).map(batch_format_fn)
-
-# Helper functions
-def evaluate(server_state):
-
-  keras_model = create_keras_model()
-  keras_model.compile(
-      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]  
-  )
-  
-  keras_model.set_weights(server_state)
-  keras_model.evaluate(central_emnist_test)
-
-# Constants
-NUM_CLIENTS = 10
-BATCH_SIZE = 20
-
-# Get the model structure JSON file.
-with open('model.json','r') as file:
-  MODEL_JSON_CONFIG = file.read()
 
 # Create the training data.
 emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
@@ -46,30 +40,56 @@ federated_train_data = [preprocess(emnist_train.create_tf_dataset_for_client(x),
   for x in client_ids
 ]
 
-def create_keras_model():
-  return tf.keras.models.model_from_json(MODEL_JSON_CONFIG)
+# 
+#  Keras helper functions
+# 
 
-  #return tf.keras.models.Sequential([
-  #    tf.keras.layers.InputLayer(input_shape=(784,)),
-  #    tf.keras.layers.Dense(10, kernel_initializer='zeros'),
-  #    tf.keras.layers.Softmax(),
-  #])
+# Create the Keras model.
+def create_keras_model(model_json_config):
+  return tf.keras.models.model_from_json(model_json_config)
 
-def model_fn():
-  keras_model = create_keras_model()
+# Run the Keras model.
+def model_fn(model_json_config, loss_function, metric_function):
+  keras_model = create_keras_model(model_json_config)
 
   return tff.learning.from_keras_model(
       keras_model,
       input_spec=federated_train_data[0].element_spec,
+      loss=getattr(tf.keras.losses, loss_function)(),
+      metrics=[getattr(tf.keras.metrics, metric_function)()])
+
+# Evaluate the Keras model.
+def evaluate(model_json_config, server_state):
+
+  keras_model = create_keras_model(model_json_config)
+  keras_model.compile(
       loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]  
+  )
+  
+  keras_model.set_weights(server_state)
+  keras_model.evaluate(central_emnist_test)
 
+# 
+#  TFF initialization
+# 
 
-# Initializes the hub by instanciating the model function.
+# Initializes the server by instanciating the model function.
 @tff.tf_computation
 def server_init():
-  model = model_fn()
+  model = model_fn(MODEL_JSON_CONFIG, LOSS_FUNCTION, METRIC_FUNCTION)
   return model.trainable_variables
+
+# TFF - Create model and do some introspection for type definition.
+model = model_fn(MODEL_JSON_CONFIG, LOSS_FUNCTION, METRIC_FUNCTION)
+model_weights_type = server_init.type_signature.result
+tf_dataset_type = tff.SequenceType(model.input_spec)
+federated_dataset_type = tff.FederatedType(tf_dataset_type, tff.CLIENTS)
+federated_server_type = tff.FederatedType(model_weights_type, tff.SERVER)
+
+# 
+#  TF + TFF server updates
+# 
 
 # TF - Updates the server model weights as the average of the client model weights.
 @tf.function
@@ -80,13 +100,15 @@ def server_update(model, mean_client_weights):
                         model_weights, mean_client_weights)
   return model_weights
 
-model_weights_type = server_init.type_signature.result
-
 # TFF - This is the tff.tf_computation version of the server update.
 @tff.tf_computation(model_weights_type)
 def server_update_fn(mean_client_weights):
-  model = model_fn()
+  model = model_fn(MODEL_JSON_CONFIG, LOSS_FUNCTION, METRIC_FUNCTION)
   return server_update(model, mean_client_weights)
+
+# 
+#  TF and TFF client updates.
+# 
 
 # TF - Performs training (using the server model weights) on the client's dataset.
 @tf.function
@@ -112,21 +134,19 @@ def client_update(model, dataset, server_weights, client_optimizer):
 
   return client_weights
 
-model = model_fn()
-tf_dataset_type = tff.SequenceType(model.input_spec)
-
 @tff.tf_computation(tf_dataset_type, model_weights_type)
 def client_update_fn(tf_dataset, server_weights):
-  model = model_fn()
-  client_optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+  model = model_fn(MODEL_JSON_CONFIG, LOSS_FUNCTION, METRIC_FUNCTION)
+  client_optimizer = getattr(tf.keras.optimizers, OPTIMIZER)(learning_rate=0.01)
   return client_update(model, tf_dataset, server_weights, client_optimizer)
+
+# 
+#  TFF federated computation definition.
+# 
 
 @tff.federated_computation
 def initialize_fn():
   return tff.federated_value(server_init(), tff.SERVER)
-
-federated_server_type = tff.FederatedType(model_weights_type, tff.SERVER)
-federated_dataset_type = tff.FederatedType(tf_dataset_type, tff.CLIENTS)
 
 @tff.federated_computation(federated_server_type, federated_dataset_type)
 def next_fn(server_weights, federated_dataset):
@@ -145,24 +165,28 @@ def next_fn(server_weights, federated_dataset):
 
   return server_weights
 
+# 
+#  Get and preprocess testing data.
+# 
+central_emnist_test = emnist_test.create_tf_dataset_from_all_clients().take(1000)
+central_emnist_test = preprocess(central_emnist_test, BATCH_SIZE)
 
+# 
+#  Run the federated computation.
+# 
 federated_algorithm = tff.templates.IterativeProcess(
     initialize_fn=initialize_fn,
     next_fn=next_fn
 )
 
-print(str(federated_algorithm.initialize.type_signature))
-print(str(federated_algorithm.next.type_signature))
-
-central_emnist_test = emnist_test.create_tf_dataset_from_all_clients().take(1000)
-central_emnist_test = preprocess(central_emnist_test, BATCH_SIZE)
-
 server_state = federated_algorithm.initialize()
-evaluate(server_state)
+evaluate(MODEL_JSON_CONFIG, server_state)
 
+# Federated averaging for 100 rounds
 for round in range(100):
   print(round)
   server_state = federated_algorithm.next(server_state, federated_train_data)
-  evaluate(server_state)
+  evaluate(MODEL_JSON_CONFIG, server_state)
 
-evaluate(server_state)
+# Do one final evaluation.
+evaluate(MODEL_JSON_CONFIG, server_state)
